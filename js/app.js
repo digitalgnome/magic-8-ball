305aa7a03b0d39d161bde239271e93cbfd9057e8
digitalgnome <digitalgnomes@gmail.com>
2016-08-05 05:30:02 -0700

    removed redundant unused code added comments to app.js

--- js/app.js
+++ js/app.js
@@ -1,18 +1,30 @@
+/*******************************************************************
+*  Magic-8-Ball Game written by Tariq Khan for Ashland JavaScript  *
+*  Meetup Code Challenge 2 on 8/4/16. Game uses jQuery and the     *
+*  GreenSock Animation Platform (GSAP)                             *
+*  code repo at https://github.com/digitalgnome/magic-8-ball       *
+********************************************************************/
+
+// game's javascript code isn't called till after DOM elements are loaded
 $(function() {
-    var answers = ["Yes", "No", "Maybe"],
+    var answers = ["Yes", "No", "Maybe"],  // initialize answers array
         returnVal = "",
         previousVal = "";
 
+    // generates random answer from the answers array
     function answer() {
         var randomNum = Math.round(Math.random() * 2);
         return answers[randomNum];
     }
 
+    // main function to generate a return answer to the eight ball
     function returnAnswer() {
         var elem;
 
+        // call to get a random answer
         returnVal = answer();
 
+        // make sure that previous answer isn't repeated
         if (previousVal === returnVal) {
             returnAnswer();
         } else if (previousVal !== returnVal) {
@@ -22,20 +34,31 @@ $(function() {
         }
     }
 
+    // use jQuery selectors to connect to and to get DOM elements
     $("#submitBtn").submit(function(event){
+        // get the string value in #questionText
         var strLen = $("#questionText").val();
 
+        // prevent default event from button submit action
+        event.preventDefault();
+
+        // using greensock JS TweenMax to reset the opacity of the #answerText to 0 instantly
         TweenMax.to("#answerText", 0, {opacity:0});
 
+        // minor form validation to check for a minimum string length of 3
         if (strLen.length < 3) {
-            event.preventDefault();
+            // set #questionText to empty string
             $("#questionText").val('');
+            // change placeholder string
             $("#questionText").attr("placeholder", "No question was asked");
         } else {
-            event.preventDefault();
+            // the fromTo method moves the ball sideways with a RoughEase affect, then x value is reset
             TweenMax.fromTo("#magic-8", 2, {x:-1}, {x:5, ease:RoughEase.ease.config({strength:8, points:20, template:Linear.easeNone, randomize:false}) , clearProps:"x"});
+            // set #questionText to empty string
             $("#questionText").val("");
+            // call to main game function
             returnAnswer();
+            // reset placeholder string
             $("#questionText").attr("placeholder", "What's your question?");
         }
     });
